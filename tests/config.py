b0c969aefe9b4c3f1cd4320b6710c0dbfb9e88f1
Hajja <h.mohamed@orsoft.fr>
2023-11-20 19:22:41 +0100

    adding test scripts

--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,4 @@
 # Configuration file for paths
-IMAGES_FOLDER = "C:/Users/User/Desktop/API/ai-engine-barcode/tests/test dataset/images"
-LABELS_FOLDER = "C:/Users/User/Desktop/API/ai-engine-barcode/tests/test dataset/labels"
-MISRECOGNIZED_FOLDER = "C:/Users/User/Desktop/API/ai-engine-barcode/tests/misrecognized"
-# images_folder = "C:/Users/User/Desktop/API/ai-engine-barcode/tests/test dataset/images"
-# labels_folder = "C:/Users/User/Desktop/API/ai-engine-barcode/tests/test dataset/labels"
-# misrecognized = "C:/Users/User/Desktop/API/ai-engine-barcode/tests/misrecognized"
+IMAGES_FOLDER = "/test dataset/images"
+LABELS_FOLDER = "./test dataset/labels"
+MISRECOGNIZED_FOLDER = "./test dataset/misrecognized"
