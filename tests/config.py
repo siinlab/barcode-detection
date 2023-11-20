c2a7a21bb0aad1a6821dad87b603991982dfc09c
Hajja <h.mohamed@orsoft.fr>
2023-11-20 19:26:36 +0100

    adding test scripts

--- tests/config.py
+++ tests/config.py
@@ -1,4 +1,4 @@
 # Configuration file for paths
-IMAGES_FOLDER = "/test dataset/images"
+IMAGES_FOLDER = "./test dataset/images"
 LABELS_FOLDER = "./test dataset/labels"
-MISRECOGNIZED_FOLDER = "./test dataset/misrecognized"
+MISRECOGNIZED_FOLDER = "./misrecognized"
